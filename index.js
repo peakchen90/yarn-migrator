246ced7e84e1a451479005b921c62cff45f7553c
peakchen90 <1558267774@qq.com>
2022-07-19 13:52:53 +0800

    update

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const path = require('path');
 const fs = require('fs-extra');
-const execa = require("execa");
+const execa = require('execa');
+
+const ROOT = process.cwd()
 
 function exit(message) {
   console.error(message);
   process.exit(1);
 }
 
-async function checkGitStatus() {
-  const {stdout} = await execa('git', ['status', '-s'], {stdio: 'pipe'})
-  if (stdout) {
-    exit('发现未提交的文件，流程终止')
-  }
-}
 
 async function checkInstallGlobalYarn() {
   try {
@@ -26,9 +22,46 @@ async function checkInstallGlobalYarn() {
   }
 }
 
+async function checkGitStatus() {
+  const {stdout} = await execa('git', ['status', '-s'], {stdio: 'pipe'})
+  if (stdout) {
+    exit('发现未提交的文件，流程终止')
+  }
+}
+
+async function upgrade() {
+  await fs.remove(path.join(ROOT, 'package-lock.json'))
+  await fs.remove(path.join(ROOT, 'node_modules'))
+  await execa('yarn', ['install'], {stdio: 'inherit'})
+  await execa('yarn', ['add', '-D', 'only-allow'], {stdio: 'inherit'})
+
+  const pkgPath = path.join(ROOT, 'package.json')
+  const pkg = fs.readJsonSync(pkgPath)
+  if (pkg.scripts && pkg.scripts.preinstall) {
+    exit('scripts `preinstall` 钩子已被占用，请先处理')
+  }
+  pkg.scripts = {
+    ...pkg.scripts,
+    preinstall: 'npx only-allow yarn'
+  }
+  fs.writeJsonSync(pkgPath, pkg, {
+    encoding: 'utf-8',
+    spaces: 2
+  })
+}
+
+async function commitAndPush() {
+  await execa('git', ['add', 'package.json', 'yarn.lock'], {stdio: 'inherit'})
+  await execa('git', ['commit', '-m', 'chore: 工程迁移至 yarn (v1) 工具 (created by yarn-migrator)'], {stdio: 'inherit'})
+  await execa('git', ['pull'], {stdio: 'inherit'})
+  await execa('git', ['push'], {stdio: 'inherit'})
+}
+
 (async () => {
-  await checkGitStatus()
   await checkInstallGlobalYarn()
+  await checkGitStatus()
+  await upgrade()
+  await commitAndPush()
 
   console.log('迁移成功')
 })()
